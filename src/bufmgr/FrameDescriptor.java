b2a5309b0137759f673e8ebdf5c1f74f11835f2f
powcoder代写 <powcoder@163.com>
2022-07-07 15:29:31 +0800

    程序代写 加微信 powcoder

---
+++ src/bufmgr/FrameDescriptor.java
@@ -0,0 +1,9 @@
+package bufmgr;
+
+
+public class FrameDescriptor 
+{
+	public int PageNumber = -1;
+	public int PinCount;
+	public boolean DirtyBit;
+}
---
+++ src/bufmgr/HashEntryNotFoundException.java
@@ -0,0 +1,16 @@
+package bufmgr;
+
+public class HashEntryNotFoundException extends BufMgrException 
+{
+
+	public HashEntryNotFoundException(String msg)
+	{
+		super(msg);
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8527332129053870174L;
+
+}
